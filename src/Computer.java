0f086939be3d45dbebb5d9fd7b9975f04ff620be
Zeeshan <Zeeshan@gmail.com>
2016-12-19 22:52:26 +0500

    It is my ist commits

---
+++ src/Computer.java
@@ -0,0 +1,10 @@
+
+public class Computer {
+	 Computer() {
+		    System.out.println("Constructor of Computer class.");
+		  }
+		 
+		  void computer_method() {
+		    System.out.println("Power gone! Shut down your PC soon...");
+		  }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		  Computer my = new Computer();
+		    Laptop your = new Laptop();
+		 
+		    my.computer_method();
+		    your.laptop_method();
+		  }
+
+	}
+
